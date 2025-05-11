d27f1bc4a7876e11dfcdd7464b3f8f22856e8d22
StefanBrunner2709 <Stefan.Brunner@student.uibk.ac.at>
2025-05-11 09:51:57 +0200

    Add mass computation and progressbar

--- src/dlr.py
+++ src/dlr.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 class LR:
     def __init__(self, U, S, V):
@@ -83,6 +84,12 @@ def Lstep(L, D1, B1, grid):
     rhs = - np.diag(grid.MU) @ L @ D1.T + 0.5 * B1 - L
     return rhs
 
+def compute_mass(lr, grid):
+    K = lr.U @ lr.S
+    rho = K @ np.trapz(lr.V, dx=grid.dmu, axis=0).T
+    M = np.trapz(rho, dx=grid.dx, axis=0)
+    return M
+
 def integrate(lr0: LR, grid: Grid, t_f: float, dt: float, option: str = "lie"):
     lr = lr0
     t = 0
@@ -90,94 +97,106 @@ def integrate(lr0: LR, grid: Grid, t_f: float, dt: float, option: str = "lie"):
     time.append(t)
     # lr_array = []     # only needed for error plots
     # lr_array.append(lr.U @ lr.S @ lr.V.T)
-    while t < t_f:
-        if (t + dt > t_f):
-            dt = t_f - t
+    # mass_array = []     # only needed for mass plots
+    # mass_array.append(compute_mass(lr, grid))
 
-        t += dt
-        time.append(t)
+    with tqdm(total=t_f/dt, desc="Running Simulation") as pbar:
 
-        if option=="lie":
-            # K step
-            C1, C2 = computeC(lr, grid)
-            K = lr.U @ lr.S
-            K += dt * RK4(K, lambda K: Kstep(K, C1, C2, grid), dt)
-            lr.U, lr.S = np.linalg.qr(K, mode="reduced")
-            lr.U /= np.sqrt(grid.dx)
-            lr.S *= np.sqrt(grid.dx)
+        while t < t_f:
 
-            # S step
-            D1 = computeD(lr, grid)
-            lr.S += dt * RK4(lr.S, lambda S: Sstep(S, C1, C2, D1), dt)
+            pbar.update(1)
 
-            # L step
-            L = lr.V @ lr.S.T
-            B1 = computeB(L, grid)
-            L += dt * RK4(L, lambda L: Lstep(L, D1, B1, grid), dt)
-            lr.V, St = np.linalg.qr(L, mode="reduced")
-            lr.S = St.T
-            lr.V /= np.sqrt(grid.dmu)
-            lr.S *= np.sqrt(grid.dmu)
-        
-        if option=="strang":
-            # 1/2 K step
-            C1, C2 = computeC(lr, grid)
-            K = lr.U @ lr.S
-            K += 0.5 * dt * RK4(K, lambda K: Kstep(K, C1, C2, grid), 0.5 * dt)
-            lr.U, lr.S = np.linalg.qr(K, mode="reduced")
-            lr.U /= np.sqrt(grid.dx)
-            lr.S *= np.sqrt(grid.dx)
+            if (t + dt > t_f):
+                dt = t_f - t
 
-            # 1/2 S step
-            D1 = computeD(lr, grid)
-            lr.S += 0.5 * dt * RK4(lr.S, lambda S: Sstep(S, C1, C2, D1), 0.5 * dt)
+            t += dt
+            time.append(t)
 
-            # L step
-            L = lr.V @ lr.S.T
-            B1 = computeB(L, grid)
-            L += dt * RK4(L, lambda L: Lstep(L, D1, B1, grid), dt)
-            lr.V, St = np.linalg.qr(L, mode="reduced")
-            lr.S = St.T
-            lr.V /= np.sqrt(grid.dmu)
-            lr.S *= np.sqrt(grid.dmu)
+            if option=="lie":
+                # K step
+                C1, C2 = computeC(lr, grid)
+                K = lr.U @ lr.S
+                K += dt * RK4(K, lambda K: Kstep(K, C1, C2, grid), dt)
+                lr.U, lr.S = np.linalg.qr(K, mode="reduced")
+                lr.U /= np.sqrt(grid.dx)
+                lr.S *= np.sqrt(grid.dx)
 
-            # 1/2 S step
-            C1, C2 = computeC(lr, grid)     # need to recalculate C1 and C2 because we changed V in L step     
-            lr.S += 0.5 * dt * RK4(lr.S, lambda S: Sstep(S, C1, C2, D1), 0.5 * dt)
+                # S step
+                D1 = computeD(lr, grid)
+                lr.S += dt * RK4(lr.S, lambda S: Sstep(S, C1, C2, D1), dt)
 
-            # 1/2 K step
-            K = lr.U @ lr.S
-            K += 0.5 * dt * RK4(K, lambda K: Kstep(K, C1, C2, grid), 0.5 * dt)
-            lr.U, lr.S = np.linalg.qr(K, mode="reduced")
-            lr.U /= np.sqrt(grid.dx)
-            lr.S *= np.sqrt(grid.dx)
+                # L step
+                L = lr.V @ lr.S.T
+                B1 = computeB(L, grid)
+                L += dt * RK4(L, lambda L: Lstep(L, D1, B1, grid), dt)
+                lr.V, St = np.linalg.qr(L, mode="reduced")
+                lr.S = St.T
+                lr.V /= np.sqrt(grid.dmu)
+                lr.S *= np.sqrt(grid.dmu)
+            
+            if option=="strang":
+                # 1/2 K step
+                C1, C2 = computeC(lr, grid)
+                K = lr.U @ lr.S
+                K += 0.5 * dt * RK4(K, lambda K: Kstep(K, C1, C2, grid), 0.5 * dt)
+                lr.U, lr.S = np.linalg.qr(K, mode="reduced")
+                lr.U /= np.sqrt(grid.dx)
+                lr.S *= np.sqrt(grid.dx)
 
-        # lr_array.append(lr.U @ lr.S @ lr.V.T)     # again only for error plots
+                # 1/2 S step
+                D1 = computeD(lr, grid)
+                lr.S += 0.5 * dt * RK4(lr.S, lambda S: Sstep(S, C1, C2, D1), 0.5 * dt)
 
-    return lr, time #, lr_array
+                # L step
+                L = lr.V @ lr.S.T
+                B1 = computeB(L, grid)
+                L += dt * RK4(L, lambda L: Lstep(L, D1, B1, grid), dt)
+                lr.V, St = np.linalg.qr(L, mode="reduced")
+                lr.S = St.T
+                lr.V /= np.sqrt(grid.dmu)
+                lr.S *= np.sqrt(grid.dmu)
+
+                # 1/2 S step
+                C1, C2 = computeC(lr, grid)     # need to recalculate C1 and C2 because we changed V in L step     
+                lr.S += 0.5 * dt * RK4(lr.S, lambda S: Sstep(S, C1, C2, D1), 0.5 * dt)
+
+                # 1/2 K step
+                K = lr.U @ lr.S
+                K += 0.5 * dt * RK4(K, lambda K: Kstep(K, C1, C2, grid), 0.5 * dt)
+                lr.U, lr.S = np.linalg.qr(K, mode="reduced")
+                lr.U /= np.sqrt(grid.dx)
+                lr.S *= np.sqrt(grid.dx)
+
+            # lr_array.append(lr.U @ lr.S @ lr.V.T)     # again only for error plots
+
+            # mass_array.append(compute_mass(lr, grid))       # again only for mass plots
+
+    return lr, time
 
 
 ''' ### Just one plot for certain rank and certain time
 
-Nx = 64
-Nmu = 64
+Nx = 256
+Nmu = 256
 dt = 1e-3
-r = 32
+r = 16
 t_f = 3.0
+t_string = "03"
 sigma = 1
 fs = 16
-savepath = "C:/Users/brunn/OneDrive/Dokumente/00_Uni/Masterarbeit/PHD_project_master_thesis/Plots_after_correction/Plots_low_rank/"
+savepath = "C:/Users/brunn/OneDrive/Dokumente/00_Uni/Masterarbeit/PHD_project_master_thesis/Plots_latex_250430/periodic_dlr/sigma1/"
+method = "strang"
 
 fig, axes = plt.subplots(1, 1, figsize=(10, 8))
 
 grid = Grid(Nx, Nmu, r)
 lr0 = setInitialCondition(grid, sigma)
 f0 = lr0.U @ lr0.S @ lr0.V.T
-lr, time = integrate(lr0, grid, t_f, dt, "strang")
+lr, time = integrate(lr0, grid, t_f, dt, method)
 f = lr.U @ lr.S @ lr.V.T
 extent = [grid.X[0], grid.X[-1], grid.MU[0], grid.MU[-1]]
 
-im = axes.imshow(f.T, extent=extent, origin='lower', aspect=0.5, vmin=0.13, vmax=0.16)
+im = axes.imshow(f.T, extent=extent, origin='lower', aspect=0.5)
 axes.set_xlabel("$x$", fontsize=fs)
 axes.set_ylabel("$\mu$", fontsize=fs, labelpad=-5)
 axes.set_xticks([0, 0.5, 1])
@@ -185,35 +204,36 @@ axes.set_yticks([-1, 0, 1])
 axes.tick_params(axis='both', labelsize=fs, pad=10)
 
 cbar_fixed = fig.colorbar(im, ax=axes)
-cbar_fixed.set_ticks([0.13, 0.145, 0.16])
+cbar_fixed.set_ticks([np.min(f), np.max(f)])
 cbar_fixed.ax.tick_params(labelsize=fs)
 
 plt.tight_layout()
-plt.savefig(savepath + "test.pdf")
+plt.savefig(savepath + "distr_funct_t" + t_string + "_" + method + "_1e-3_r16.pdf")
 '''
-
 
 ''' ### 4 plots, same time, different ranks
 
-Nx = 64
-Nmu = 64
+Nx = 256
+Nmu = 256
 dt = 1e-3
 r_array = [4, 8, 16, 32]
 t_f = 1.0
+t_string = "01"
 sigma = 1
 fs = 16
-savepath = "C:/Users/brunn/OneDrive/Dokumente/00_Uni/Masterarbeit/PHD_project_master_thesis/Plots_after_correction/Plots_low_rank/"
+savepath = "C:/Users/brunn/OneDrive/Dokumente/00_Uni/Masterarbeit/PHD_project_master_thesis/Plots_latex_250430/periodic_dlr/sigma1/"
+method = "strang"
 
 fig, axes = plt.subplots(2, 2, figsize=(10, 8))
 
 grid = Grid(Nx, Nmu, r_array[0])
 lr0 = setInitialCondition(grid, sigma)
 f0 = lr0.U @ lr0.S @ lr0.V.T
-lr, time = integrate(lr0, grid, t_f, dt)
+lr, time = integrate(lr0, grid, t_f, dt, method)
 f = lr.U @ lr.S @ lr.V.T
 extent = [grid.X[0], grid.X[-1], grid.MU[0], grid.MU[-1]]
 
-im1 = axes[0, 0].imshow(f.T, extent=extent, origin='lower', aspect=0.5, vmin=0.13, vmax=0.16)
+im1 = axes[0, 0].imshow(f.T, extent=extent, origin='lower', aspect=0.5, vmin=0.143, vmax=0.155)
 axes[0, 0].set_title("$r=$" + str(r_array[0]), fontsize=fs)
 axes[0, 0].set_xlabel("$x$", fontsize=fs)
 axes[0, 0].set_ylabel("$\mu$", fontsize=fs, labelpad=-5)
@@ -224,11 +244,11 @@ axes[0, 0].tick_params(axis='both', labelsize=fs, pad=10)
 grid = Grid(Nx, Nmu, r_array[1])
 lr0 = setInitialCondition(grid, sigma)
 f0 = lr0.U @ lr0.S @ lr0.V.T
-lr, time = integrate(lr0, grid, t_f, dt)
+lr, time = integrate(lr0, grid, t_f, dt, method)
 f = lr.U @ lr.S @ lr.V.T
 extent = [grid.X[0], grid.X[-1], grid.MU[0], grid.MU[-1]]
 
-im2 = axes[0, 1].imshow(f.T, extent=extent, origin='lower', aspect=0.5, vmin=0.13, vmax=0.16)
+im2 = axes[0, 1].imshow(f.T, extent=extent, origin='lower', aspect=0.5, vmin=0.143, vmax=0.155)
 axes[0, 1].set_title("$r=$" + str(r_array[1]), fontsize=fs)
 axes[0, 1].set_xlabel("$x$", fontsize=fs)
 axes[0, 1].set_ylabel("$\mu$", fontsize=fs, labelpad=-5)
@@ -239,11 +259,11 @@ axes[0, 1].tick_params(axis='both', labelsize=fs, pad=10)
 grid = Grid(Nx, Nmu, r_array[2])
 lr0 = setInitialCondition(grid, sigma)
 f0 = lr0.U @ lr0.S @ lr0.V.T
-lr, time = integrate(lr0, grid, t_f, dt)
+lr, time = integrate(lr0, grid, t_f, dt, method)
 f = lr.U @ lr.S @ lr.V.T
 extent = [grid.X[0], grid.X[-1], grid.MU[0], grid.MU[-1]]
 
-im3 = axes[1, 0].imshow(f.T, extent=extent, origin='lower', aspect=0.5, vmin=0.13, vmax=0.16)
+im3 = axes[1, 0].imshow(f.T, extent=extent, origin='lower', aspect=0.5, vmin=0.143, vmax=0.155)
 axes[1, 0].set_title("$r=$" + str(r_array[2]), fontsize=fs)
 axes[1, 0].set_xlabel("$x$", fontsize=fs)
 axes[1, 0].set_ylabel("$\mu$", fontsize=fs, labelpad=-5)
@@ -254,11 +274,11 @@ axes[1, 0].tick_params(axis='both', labelsize=fs, pad=10)
 grid = Grid(Nx, Nmu, r_array[3])
 lr0 = setInitialCondition(grid, sigma)
 f0 = lr0.U @ lr0.S @ lr0.V.T
-lr, time = integrate(lr0, grid, t_f, dt)
+lr, time = integrate(lr0, grid, t_f, dt, method)
 f = lr.U @ lr.S @ lr.V.T
 extent = [grid.X[0], grid.X[-1], grid.MU[0], grid.MU[-1]]
 
-im4 = axes[1, 1].imshow(f.T, extent=extent, origin='lower', aspect=0.5, vmin=0.13, vmax=0.16)
+im4 = axes[1, 1].imshow(f.T, extent=extent, origin='lower', aspect=0.5, vmin=0.143, vmax=0.155)
 axes[1, 1].set_title("$r=$" + str(r_array[3]), fontsize=fs)
 axes[1, 1].set_xlabel("$x$", fontsize=fs)
 axes[1, 1].set_ylabel("$\mu$", fontsize=fs, labelpad=-5)
@@ -267,23 +287,22 @@ axes[1, 1].set_yticks([-1, 0, 1])
 axes[1, 1].tick_params(axis='both', labelsize=fs, pad=10)
 
 cbar_fixed1 = fig.colorbar(im1, ax=axes[0, 0])
-cbar_fixed1.set_ticks([0.13, 0.145, 0.16])
+cbar_fixed1.set_ticks([0.144, 0.149, 0.154])
 cbar_fixed1.ax.tick_params(labelsize=fs)
 cbar_fixed2 = fig.colorbar(im2, ax=axes[0, 1])
-cbar_fixed2.set_ticks([0.13, 0.145, 0.16])
+cbar_fixed2.set_ticks([0.144, 0.149, 0.154])
 cbar_fixed2.ax.tick_params(labelsize=fs)
 cbar_fixed3 = fig.colorbar(im3, ax=axes[1, 0])
-cbar_fixed3.set_ticks([0.13, 0.145, 0.16])
+cbar_fixed3.set_ticks([0.144, 0.149, 0.154])
 cbar_fixed3.ax.tick_params(labelsize=fs)
 cbar_fixed4 = fig.colorbar(im4, ax=axes[1, 1])
-cbar_fixed4.set_ticks([0.13, 0.145, 0.16])
+cbar_fixed4.set_ticks([0.144, 0.149, 0.154])
 cbar_fixed4.ax.tick_params(labelsize=fs)
 
 plt.tight_layout()
 plt.subplots_adjust(wspace=0.3, hspace=0.6)
-plt.savefig(savepath + "test.pdf")
+plt.savefig(savepath + "distr_funct_different_ranks_t" + t_string + "_" + method + "_1e-3.pdf")
 '''
-
 
 ''' ### 4 plots, same rank, different times
 
@@ -294,18 +313,19 @@ r = 16
 t_f_array = [0.5, 1.0, 2.0, 3.0]
 sigma = 1
 fs = 16
-savepath = "C:/Users/brunn/OneDrive/Dokumente/00_Uni/Masterarbeit/PHD_project_master_thesis/Plots_after_correction/Plots_low_rank/"
+savepath = "C:/Users/brunn/OneDrive/Dokumente/00_Uni/Masterarbeit/PHD_project_master_thesis/Plots_latex_250430/periodic_dlr/sigma1/"
+method = "strang"
 
 fig, axes = plt.subplots(2, 2, figsize=(10, 8))
 
 grid = Grid(Nx, Nmu, r)
 lr0 = setInitialCondition(grid, sigma)
 f0 = lr0.U @ lr0.S @ lr0.V.T
-lr, time = integrate(lr0, grid, t_f_array[0], dt)
+lr, time = integrate(lr0, grid, t_f_array[0], dt, method)
 f = lr.U @ lr.S @ lr.V.T
 extent = [grid.X[0], grid.X[-1], grid.MU[0], grid.MU[-1]]
 
-im1 = axes[0, 0].imshow(f.T, extent=extent, origin='lower', aspect=0.5, vmin=0.13, vmax=0.16)
+im1 = axes[0, 0].imshow(f.T, extent=extent, origin='lower', aspect=0.5, vmin=0.138, vmax=0.158)
 axes[0, 0].set_title("$t=$" + str(t_f_array[0]), fontsize=fs)
 axes[0, 0].set_xlabel("$x$", fontsize=fs)
 axes[0, 0].set_ylabel("$\mu$", fontsize=fs, labelpad=-5)
@@ -316,11 +336,11 @@ axes[0, 0].tick_params(axis='both', labelsize=fs, pad=10)
 grid = Grid(Nx, Nmu, r)
 lr0 = setInitialCondition(grid, sigma)
 f0 = lr0.U @ lr0.S @ lr0.V.T
-lr, time = integrate(lr0, grid, t_f_array[1], dt)
+lr, time = integrate(lr0, grid, t_f_array[1], dt, method)
 f = lr.U @ lr.S @ lr.V.T
 extent = [grid.X[0], grid.X[-1], grid.MU[0], grid.MU[-1]]
 
-im2 = axes[0, 1].imshow(f.T, extent=extent, origin='lower', aspect=0.5, vmin=0.13, vmax=0.16)
+im2 = axes[0, 1].imshow(f.T, extent=extent, origin='lower', aspect=0.5, vmin=0.143, vmax=0.155)
 axes[0, 1].set_title("$t=$" + str(t_f_array[1]), fontsize=fs)
 axes[0, 1].set_xlabel("$x$", fontsize=fs)
 axes[0, 1].set_ylabel("$\mu$", fontsize=fs, labelpad=-5)
@@ -331,11 +351,11 @@ axes[0, 1].tick_params(axis='both', labelsize=fs, pad=10)
 grid = Grid(Nx, Nmu, r)
 lr0 = setInitialCondition(grid, sigma)
 f0 = lr0.U @ lr0.S @ lr0.V.T
-lr, time = integrate(lr0, grid, t_f_array[2], dt)
+lr, time = integrate(lr0, grid, t_f_array[2], dt, method)
 f = lr.U @ lr.S @ lr.V.T
 extent = [grid.X[0], grid.X[-1], grid.MU[0], grid.MU[-1]]
 
-im3 = axes[1, 0].imshow(f.T, extent=extent, origin='lower', aspect=0.5, vmin=0.13, vmax=0.16)
+im3 = axes[1, 0].imshow(f.T, extent=extent, origin='lower', aspect=0.5, vmin=0.1484, vmax=0.1526)
 axes[1, 0].set_title("$t=$" + str(t_f_array[2]), fontsize=fs)
 axes[1, 0].set_xlabel("$x$", fontsize=fs)
 axes[1, 0].set_ylabel("$\mu$", fontsize=fs, labelpad=-5)
@@ -346,11 +366,11 @@ axes[1, 0].tick_params(axis='both', labelsize=fs, pad=10)
 grid = Grid(Nx, Nmu, r)
 lr0 = setInitialCondition(grid, sigma)
 f0 = lr0.U @ lr0.S @ lr0.V.T
-lr, time = integrate(lr0, grid, t_f_array[3], dt)
+lr, time = integrate(lr0, grid, t_f_array[3], dt, method)
 f = lr.U @ lr.S @ lr.V.T
 extent = [grid.X[0], grid.X[-1], grid.MU[0], grid.MU[-1]]
 
-im4 = axes[1, 1].imshow(f.T, extent=extent, origin='lower', aspect=0.5, vmin=0.13, vmax=0.16)
+im4 = axes[1, 1].imshow(f.T, extent=extent, origin='lower', aspect=0.5, vmin=0.1505, vmax=0.1521)
 axes[1, 1].set_title("$t=$" + str(t_f_array[3]), fontsize=fs)
 axes[1, 1].set_xlabel("$x$", fontsize=fs)
 axes[1, 1].set_ylabel("$\mu$", fontsize=fs, labelpad=-5)
@@ -359,19 +379,69 @@ axes[1, 1].set_yticks([-1, 0, 1])
 axes[1, 1].tick_params(axis='both', labelsize=fs, pad=10)
 
 cbar_fixed1 = fig.colorbar(im1, ax=axes[0, 0])
-cbar_fixed1.set_ticks([0.13, 0.145, 0.16])
+cbar_fixed1.set_ticks([0.139, 0.148, 0.157])
 cbar_fixed1.ax.tick_params(labelsize=fs)
 cbar_fixed2 = fig.colorbar(im2, ax=axes[0, 1])
-cbar_fixed2.set_ticks([0.13, 0.145, 0.16])
+cbar_fixed2.set_ticks([0.144, 0.149, 0.154])
 cbar_fixed2.ax.tick_params(labelsize=fs)
 cbar_fixed3 = fig.colorbar(im3, ax=axes[1, 0])
-cbar_fixed3.set_ticks([0.13, 0.145, 0.16])
+cbar_fixed3.set_ticks([0.149, 0.152])
 cbar_fixed3.ax.tick_params(labelsize=fs)
 cbar_fixed4 = fig.colorbar(im4, ax=axes[1, 1])
-cbar_fixed4.set_ticks([0.13, 0.145, 0.16])
+cbar_fixed4.set_ticks([0.151, 0.152])
 cbar_fixed4.ax.tick_params(labelsize=fs)
 
 plt.tight_layout()
 plt.subplots_adjust(wspace=0.3, hspace=0.6)
-plt.savefig(savepath + "test.pdf")
+plt.savefig(savepath + "distr_funct_different_times_r" + str(r) + "_" + method + "_1e-3.pdf")
+'''
+
+''' ### Values for colorbar sigma 1
+
+im1 = axes[0, 0].imshow(f.T, extent=extent, origin='lower', aspect=0.5, vmin=0.138, vmax=0.158)
+im2 = axes[0, 1].imshow(f.T, extent=extent, origin='lower', aspect=0.5, vmin=0.143, vmax=0.155)
+im3 = axes[1, 0].imshow(f.T, extent=extent, origin='lower', aspect=0.5, vmin=0.1484, vmax=0.1526)
+im4 = axes[1, 1].imshow(f.T, extent=extent, origin='lower', aspect=0.5, vmin=0.1505, vmax=0.1521)
+
+cbar_fixed1 = fig.colorbar(im1, ax=axes[0, 0])
+cbar_fixed1.set_ticks([0.139, 0.148, 0.157])
+cbar_fixed1.ax.tick_params(labelsize=fs)
+cbar_fixed2 = fig.colorbar(im2, ax=axes[0, 1])
+cbar_fixed2.set_ticks([0.144, 0.149, 0.154])
+cbar_fixed2.ax.tick_params(labelsize=fs)
+cbar_fixed3 = fig.colorbar(im3, ax=axes[1, 0])
+cbar_fixed3.set_ticks([0.149, 0.152])
+cbar_fixed3.ax.tick_params(labelsize=fs)
+cbar_fixed4 = fig.colorbar(im4, ax=axes[1, 1])
+cbar_fixed4.set_ticks([0.151, 0.152])
+cbar_fixed4.ax.tick_params(labelsize=fs)
+'''
+
+''' ### Plot mass over time
+
+Nx = 256
+Nmu = 256
+dt = 1e-3
+r = 16
+t_f = 3.0
+t_string = "03"
+sigma = 1
+fs = 16
+savepath = "C:/Users/brunn/OneDrive/Dokumente/00_Uni/Masterarbeit/PHD_project_master_thesis/Plots_latex_250430/periodic_dlr/sigma1/"
+method = "strang"
+
+fig, axes = plt.subplots(1, 1, figsize=(10, 8))
+
+grid = Grid(Nx, Nmu, r)
+lr0 = setInitialCondition(grid, sigma)
+f0 = lr0.U @ lr0.S @ lr0.V.T
+lr, time, mass = integrate(lr0, grid, t_f, dt, method)
+f = lr.U @ lr.S @ lr.V.T
+extent = [grid.X[0], grid.X[-1], grid.MU[0], grid.MU[-1]]
+
+plt.plot(time, mass)
+plt.xlabel("$t$")
+plt.ylabel("mass")
+
+plt.savefig(savepath + "mass_over_time_" + method + "_1e-3_r" + str(r) + "_sigma" + str(sigma) + ".pdf")
 '''
