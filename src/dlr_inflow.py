ddf76379e926943b1bb1cc2217d2f355e6e2651c
Stefan Brunner <Stefan.Brunner@student.uibk.ac.at>
2025-06-18 10:45:47 +0200

    Changes for video

--- src/dlr_inflow.py
+++ src/dlr_inflow.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import matplotlib.gridspec as gridspec
 
 class LR:
     def __init__(self, U, S, V):
@@ -43,7 +44,7 @@ def RK4(f, rhs, dt):
 def computeF_b(f, grid, t):
     
     F_b = np.zeros((2, len(grid.MU)))
-    
+    """
     # values from inflow:
     if grid.MU[191] > 0:     # leftmost entries are for negative mu, rightmost por positive mu
         F_b[0, 191] = np.tanh(t)
@@ -55,7 +56,7 @@ def computeF_b(f, grid, t):
             F_b[0, i] = np.tanh(t)
         elif grid.MU[i] < 0:
             F_b[1, i] = np.tanh(t)
-    """
+    
     #Values from extrapolation from f:          # not completely sure about indices
     for i in range(int(grid.Nx/2)):
         F_b[0, i] = f[0,i] - (f[1,i]-f[0,i])/grid.dx * grid.X[0]
@@ -68,7 +69,7 @@ def computeK_bdry(lr, grid, t):
 
     e_vec_left = np.zeros([len(grid.MU)])
     e_vec_right = np.zeros([len(grid.MU)])
-    
+    """
     if grid.MU[191] > 0:
         e_vec_left[191] = np.tanh(t)
     elif grid.MU[191] < 0:
@@ -81,7 +82,7 @@ def computeK_bdry(lr, grid, t):
         elif grid.MU[i] < 0:
             # e_vec_right[i] = np.exp(-(grid.MU[i])**2/2)
             e_vec_right[i] = np.tanh(t)
-    """
+    
     int_exp_left = (e_vec_left @ lr.V) * grid.dmu   # compute integral from inflow, contains information from inflow from every K_j
     int_exp_right = (e_vec_right @ lr.V) * grid.dmu
 
@@ -169,6 +170,7 @@ def integrate(lr0: LR, grid: Grid, t_f: float, dt: float, option: str = "lie", t
     f = lr.U @ lr.S @ lr.V.T
     #rank.append(np.linalg.matrix_rank(f, tol))
     adapt_rank.append(grid.r)
+    vid_frame = 0
 
     with tqdm(total=t_f/dt, desc="Running Simulation") as pbar:
 
@@ -286,6 +288,48 @@ def integrate(lr0: LR, grid: Grid, t_f: float, dt: float, option: str = "lie", t
             #rank.append(np.linalg.matrix_rank(f, tol))
             adapt_rank.append(grid.r)
 
+
+
+            ### Do the plotting for video
+            
+            if np.round(t/dt) % 50 == 0:
+                fs = 22
+                savepath = "/home/stefan/DLR-rt/plots/"
+                extent = [grid.X[0], grid.X[-1], grid.MU[0], grid.MU[-1]]
+                f = lr.U @ lr.S @ lr.V.T
+
+                fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 6))
+
+                #im = ax1.imshow(f.T, extent=extent, origin='lower', aspect=0.5)
+                im = ax1.imshow(f.T, extent=extent, origin='lower', aspect=0.5, vmin=0.0, vmax=1.0)
+                ax1.set_xlabel("$x$", fontsize=fs)
+                ax1.set_ylabel("$\mu$", fontsize=fs, labelpad=-5)
+                ax1.set_xticks([0, 0.5, 1])
+                ax1.set_yticks([-1, 0, 1])
+                ax1.tick_params(axis='both', labelsize=fs, pad=20)
+                ax1.set_title("$f(t,x,\mu)$", fontsize=fs)
+
+                cbar_fixed = fig.colorbar(im, ax=ax1, shrink=1)
+                #cbar_fixed.set_ticks([np.ceil(np.min(f)*10000)/10000, np.floor(np.max(f)*10000)/10000])
+                cbar_fixed.set_ticks([0, 0.5, 1])
+                cbar_fixed.ax.tick_params(labelsize=fs)
+
+                ax2.plot(time, adapt_rank)
+                ax2.set_xlabel("$t$", fontsize=22)
+                ax2.set_ylabel("rank $r(t)$", fontsize=22)
+                ax2.tick_params(axis='both', labelsize=22)
+                ax2.set_yticks([5,6,7,8,9,10])
+                ax2.set_xticks([0,1,2])
+                ax2.margins(x=0)
+                ax2.set_xlim(0, 2)
+
+                vid_frame += 1
+
+                plt.tight_layout()
+                plt.savefig(savepath + f"frame_{vid_frame:04d}.png")
+            
+
+
     return lr, time, adapt_rank
 
 
@@ -297,11 +341,9 @@ Nmu = 256
 dt = 1e-4
 r = 5
 t_f = 2.0
-fs = 30
+fs = 22
 method = "lie"
-savepath = "C:/Users/brunn/OneDrive/Dokumente/00_Uni/Masterarbeit/PHD_project_master_thesis/Plots_latex_250430/inflow/"
-
-fig, axes = plt.subplots(1, 1, figsize=(10, 8))
+savepath = "/home/stefan/DLR-rt/plots/"
 
 grid = Grid(Nx, Nmu, r)
 lr0 = setInitialCondition(grid)
@@ -311,29 +353,31 @@ extent = [grid.X[0], grid.X[-1], grid.MU[0], grid.MU[-1]]
 lr, time, rank = integrate(lr0, grid, t_f, dt, option=method, tol_sing_val=1e-5, drop_tol=1e-5)
 f = lr.U @ lr.S @ lr.V.T
 '''
-#im = axes.imshow(f.T, extent=extent, origin='lower', aspect=0.5)
-im = axes.imshow(f.T, extent=extent, origin='lower', aspect=0.5, vmin=0.0, vmax=1.0)
-axes.set_xlabel("$x$", fontsize=fs)
-axes.set_ylabel("$\mu$", fontsize=fs, labelpad=-5)
-axes.set_xticks([0, 0.5, 1])
-axes.set_yticks([-1, 0, 1])
-axes.tick_params(axis='both', labelsize=fs, pad=20)
-axes.set_title("$t=$" + str(t_f), fontsize=fs)
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 6))
 
-cbar_fixed = fig.colorbar(im, ax=axes)
+#im = ax1.imshow(f.T, extent=extent, origin='lower', aspect=0.5)
+im = ax1.imshow(f.T, extent=extent, origin='lower', aspect=0.5, vmin=0.0, vmax=1.0)
+ax1.set_xlabel("$x$", fontsize=fs)
+ax1.set_ylabel("$\mu$", fontsize=fs, labelpad=-5)
+ax1.set_xticks([0, 0.5, 1])
+ax1.set_yticks([-1, 0, 1])
+ax1.tick_params(axis='both', labelsize=fs, pad=20)
+ax1.set_title("$f(t,x,\mu)$", fontsize=fs)
+
+cbar_fixed = fig.colorbar(im, ax=ax1, shrink=1)
 #cbar_fixed.set_ticks([np.ceil(np.min(f)*10000)/10000, np.floor(np.max(f)*10000)/10000])
 cbar_fixed.set_ticks([0, 0.5, 1])
 cbar_fixed.ax.tick_params(labelsize=fs)
 
+ax2.plot(time, rank)
+ax2.set_xlabel("$t$", fontsize=22)
+ax2.set_ylabel("rank $r(t)$", fontsize=22)
+ax2.tick_params(axis='both', labelsize=22)
+ax2.set_yticks([5,6,7,8,9,10])
+ax2.set_xticks([0,1,2])
+ax2.margins(x=0)
+ax2.set_xlim(0, 2)
+
 plt.tight_layout()
-plt.savefig(savepath + "distr_funct_initalltanh_fixedcol_t" + str(t_f) + "_" + method + "_" + str(dt) + "_adaptrank_" + str(Nx) + "x" + str(Nmu) + ".pdf")
+plt.savefig(savepath + "test.pdf")
 '''
-fig, ax = plt.subplots()
-ax.plot(time, rank)
-ax.set_xlabel("$t$", fontsize=22)
-ax.set_ylabel("rank $r(t)$", fontsize=22)
-ax.tick_params(axis='both', labelsize=22)
-ax.set_yticks([5])
-ax.margins(x=0)
-plt.tight_layout()
-plt.savefig(savepath + "adaptive_rank_initsingletanh_t" + str(t_f) + "_" + method + "_" + str(dt) + "_adaptrank_" + str(Nx) + "x" + str(Nmu) + ".pdf")
