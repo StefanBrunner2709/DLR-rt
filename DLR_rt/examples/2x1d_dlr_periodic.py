4e9397c7781df1e4a10f9912e6b0c947c0d84ac9
Stefan Brunner <Stefan.Brunner@student.uibk.ac.at>
2025-07-08 14:03:34 +0200

    Add Frobenius norm, add different plots

--- DLR_rt/examples/2x1d_dlr_periodic.py
+++ DLR_rt/examples/2x1d_dlr_periodic.py
@@ -39,7 +39,7 @@ Ny = 32
 Nphi = 32
 dt = 1e-3
 r = 8
-t_f = 0.25
+t_f = 1.0
 fs = 16
 savepath = "plots/"
 method = "lie"
@@ -88,4 +88,60 @@ cbar_fixed.set_ticks([np.min(rho_matrix), np.max(rho_matrix)])
 cbar_fixed.ax.tick_params(labelsize=fs)
 
 plt.tight_layout()
-plt.savefig(savepath + "2x1d_rho_final_phipiquarter.pdf")
+plt.savefig(savepath + "2x1d_rho_final.pdf")
+
+
+### Calculate Frobenius norm
+
+fro_norm = np.linalg.norm(f0-f, 'fro')
+print("Frobenius norm f(t=0) - f(t=1): ", fro_norm)
+
+
+### Plots integrating over x:
+
+# Reshape into (n_y, n_x, n_phi)
+f0_reshaped = f0.reshape(Ny, Nx, Nphi)
+
+# Apply trapezoidal integration along the x-axis (axis=1)
+f0_integrated = np.trapezoid(f0_reshaped, grid.X, axis=1)  # Result shape: (n_y, n_phi)
+
+fig, axes = plt.subplots(1, 1, figsize=(10, 8))
+
+extent = [grid.PHI[0], grid.PHI[-1], grid.Y[0], grid.Y[-1]]
+
+im = axes.imshow(f0_integrated, extent=extent, origin='lower', aspect = "auto")
+axes.set_xlabel(r"$\phi$", fontsize=fs)
+axes.set_ylabel("y", fontsize=fs, labelpad=-5)
+axes.set_yticks([0, 0.5, 1])
+axes.tick_params(axis='both', labelsize=fs, pad=10)
+
+cbar_fixed = fig.colorbar(im, ax=axes)
+cbar_fixed.set_ticks([np.min(f0_integrated), np.max(f0_integrated)])
+cbar_fixed.ax.tick_params(labelsize=fs)
+
+plt.tight_layout()
+plt.savefig(savepath + "2x1d_collisions_yphi_initial_piquarter_t10.pdf")
+
+
+# Reshape into (n_y, n_x, n_phi)
+f_reshaped = f.reshape(Ny, Nx, Nphi)
+
+# Apply trapezoidal integration along the x-axis (axis=1)
+f_integrated = np.trapezoid(f_reshaped, grid.X, axis=1)  # Result shape: (n_y, n_phi)
+
+fig, axes = plt.subplots(1, 1, figsize=(10, 8))
+
+extent = [grid.PHI[0], grid.PHI[-1], grid.Y[0], grid.Y[-1]]
+
+im = axes.imshow(f_integrated, extent=extent, origin='lower', aspect = "auto")
+axes.set_xlabel(r"$\phi$", fontsize=fs)
+axes.set_ylabel("y", fontsize=fs, labelpad=-5)
+axes.set_yticks([0, 0.5, 1])
+axes.tick_params(axis='both', labelsize=fs, pad=10)
+
+cbar_fixed = fig.colorbar(im, ax=axes)
+cbar_fixed.set_ticks([np.min(f_integrated), np.max(f_integrated)])
+cbar_fixed.ax.tick_params(labelsize=fs)
+
+plt.tight_layout()
+plt.savefig(savepath + "2x1d_collisions_yphi_final_piquarter_t10.pdf")
