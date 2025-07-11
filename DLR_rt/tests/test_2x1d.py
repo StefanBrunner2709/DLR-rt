f2a2f5d3c67df2c2cd9bbfaa52cfd1266e88179c
Stefan Brunner <Stefan.Brunner@student.uibk.ac.at>
2025-07-11 10:48:05 +0200

    Add test. ToDo: still change, s.t. only advection

---
+++ DLR_rt/tests/test_2x1d.py
@@ -0,0 +1,35 @@
+import unittest
+import numpy as np
+
+
+from DLR_rt.examples.dlr_2x1d_periodic import integrate
+from DLR_rt.src.grid import Grid_2x1d
+from DLR_rt.src.initial_condition import setInitialCondition_2x1d_lr
+
+# ToDo: With large collisions term this probably does not work. After rewriting code,
+#       change this as well, usch that we only look at adveciton, not collision
+
+class ModelTestCase(unittest.TestCase):
+    def setUp(self):
+        dt = 1e-3
+        r = 8
+        t_f = 1.0
+        N = 16      # grid size for x, mu and phi
+
+        grid1 = Grid_2x1d(N, N, N, r)
+        lr01 = setInitialCondition_2x1d_lr(grid1)
+        f01 = lr01.U @ lr01.S @ lr01.V.T
+        lr1, time = integrate(lr01, grid1, t_f, dt)
+        f1 = lr1.U @ lr1.S @ lr1.V.T
+
+        self.fro_norm1 = np.linalg.norm(f01-f1, 'fro')
+
+    def test_frobenius_norm(self):
+        '''
+        Test if error in frobenius norm from inital value and end value at t=1 is not huge
+        '''
+        self.assertGreater(1.0, self.fro_norm1)
+
+
+if __name__ == "__main__":
+    unittest.main()
