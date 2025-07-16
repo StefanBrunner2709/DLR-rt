34a4c2e1f807b82be7077850785c44b01e36d149
Stefan Brunner <Stefan.Brunner@student.uibk.ac.at>
2025-07-16 14:09:06 +0200

    Add new 2x1d grid for domain decomposition

--- DLR_rt/src/grid.py
+++ DLR_rt/src/grid.py
@@ -96,19 +96,53 @@ class Grid_2x1d:
     _coeff
         1/epsilon for radiative transfer equation on this domain.
     """
-    def __init__(self, _Nx: int, _Ny: int, _Nphi: int, _r: int = 5, _coeff : float = 1.0):
+    def __init__(self, _Nx: int, _Ny: int, _Nphi: int, _r: int = 5, _X = None, _coeff : float = 1.0, option : str = "no_dd"):
         self.Nx = _Nx
         self.Ny = _Ny
         self.Nphi = _Nphi
         self.r = _r
         self.coeff = _coeff
 
-        self.X = np.linspace(0.0, 1.0, self.Nx, endpoint=False)          # Point 0 is on the grid, point 1 is not on the grid
-        self.Y = np.linspace(0.0, 1.0, self.Ny, endpoint=False)          # Point 0 is on the grid, point 1 is not on the grid
+        if option == "no_dd":
+            self.X = np.linspace(0.0, 1.0, self.Nx, endpoint=False)          # Point 0 is on the grid, point 1 is not on the grid
+            self.Y = np.linspace(0.0, 1.0, self.Ny, endpoint=False)          # Point 0 is on the grid, point 1 is not on the grid
+        elif option == "dd":
+            if _X is None:
+                self.X = np.linspace(1/(2*self.Nx), 1 - 1/(2*self.Nx), self.Nx, endpoint=True) # Point 0 and 1 are not on the grid, spacing to first gridpoint: delta_x/2
+            else:
+                self.X = _X
+            self.Y = np.linspace(1/(2*self.Ny), 1 - 1/(2*self.Ny), self.Ny, endpoint=True) # Point 0 and 1 are not on the grid, spacing to first gridpoint: delta_x/2
+
         self.PHI = np.linspace(0.0, 2*np.pi, self.Nphi, endpoint=False)        # 2*pi is the same angle as 0
         
         self.dx = self.X[1] - self.X[0]
         self.dy = self.Y[1] - self.Y[0]
         self.dphi = self.PHI[1] - self.PHI[0]
 
-    # ToDo: Add function to split domains
+    def split_x(self, _coeff_left = None, _coeff_right = None):
+        """
+        Split domain into 2 subdomains in x dimension.
+
+        Split the domain into 2 subdomains by dividing the domain in half in the middle of the X grid.
+
+        Parameters
+        ----------
+        _coeff_left
+            1/epsilon for radiative transfer equation on left subdomain. If None, value from whole domain is taken.
+        _coeff_right
+            1/epsilon for radiative transfer equation on right subdomain. If None, value from whole domain is taken.
+        """
+        if _coeff_left is None:
+            _coeff_left = self.coeff
+        if _coeff_right is None:
+            _coeff_right = self.coeff
+
+        # Split grid
+        X_left = self.X[:int(self.Nx/2)]
+        X_right = self.X[int(self.Nx/2):]
+
+        # Create new Grid instances for left and right
+        left_grid = Grid_2x1d(int(self.Nx/2), self.Ny, self.Nphi, self.r, _X=X_left, _coeff = _coeff_left, option = "dd")
+        right_grid = Grid_2x1d(int(self.Nx/2), self.Ny, self.Nphi, self.r, _X=X_right, _coeff = _coeff_right, option = "dd")
+
+        return left_grid, right_grid
