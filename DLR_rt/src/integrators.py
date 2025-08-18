Fix scaling 2x1d (ToDo: check if correct)

DLR_rt/src/integrators.py
@@ -87,8 +87,12 @@ def PSI_lie(lr, grid, dt, F_b=None, DX=None, DY=None, dimensions="1x1d",
         dt,
     )
     lr.U, lr.S = np.linalg.qr(K, mode="reduced")
-    lr.U /= np.sqrt(grid.dx)
-    lr.S *= np.sqrt(grid.dx)
+    if dimensions == "1x1d":
+        lr.U /= np.sqrt(grid.dx)
+        lr.S *= np.sqrt(grid.dx)
+    elif dimensions == "2x1d":
+        lr.U /= (np.sqrt(grid.dx) * np.sqrt(grid.dy))
+        lr.S *= (np.sqrt(grid.dx) * np.sqrt(grid.dy))
 
     # S step
     D1 = computeD(lr, grid, F_b, DX=DX, DY=DY, 
@@ -244,8 +248,8 @@ def PSI_splitting_lie(
     K = lr.U @ lr.S
     K += dt * RK4(K, lambda K: Kstep1(C1, grid, lr, F_b, F_b_top_bottom, DX, DY), dt)
     lr.U, lr.S = np.linalg.qr(K, mode="reduced")
-    lr.U /= np.sqrt(grid.dx)
-    lr.S *= np.sqrt(grid.dx)
+    lr.U /= (np.sqrt(grid.dx) * np.sqrt(grid.dy))
+    lr.S *= (np.sqrt(grid.dx) * np.sqrt(grid.dy))
 
     # S step
     D1 = computeD(
@@ -279,8 +283,8 @@ def PSI_splitting_lie(
     K = lr.U @ lr.S
     K += dt * RK4(K, lambda K: Kstep2(C1, grid, lr, F_b, F_b_top_bottom, DX, DY), dt)
     lr.U, lr.S = np.linalg.qr(K, mode="reduced")
-    lr.U /= np.sqrt(grid.dx)
-    lr.S *= np.sqrt(grid.dx)
+    lr.U /= (np.sqrt(grid.dx) * np.sqrt(grid.dy))
+    lr.S *= (np.sqrt(grid.dx) * np.sqrt(grid.dy))
 
     # S step
     D1 = computeD(
@@ -308,8 +312,8 @@ def PSI_splitting_lie(
     K = lr.U @ lr.S
     K += dt * RK4(K, lambda K: Kstep3(K, C2, grid), dt)
     lr.U, lr.S = np.linalg.qr(K, mode="reduced")
-    lr.U /= np.sqrt(grid.dx)
-    lr.S *= np.sqrt(grid.dx)
+    lr.U /= (np.sqrt(grid.dx) * np.sqrt(grid.dy))
+    lr.S *= (np.sqrt(grid.dx) * np.sqrt(grid.dy))
 
     # S step
     lr.S += dt * RK4(lr.S, lambda S: Sstep3(S, C2, grid), dt)
@@ -375,8 +379,8 @@ def PSI_splitting_strang(
     K = lr.U @ lr.S
     K += 0.5 * dt * RK4(K, lambda K: Kstep1(C1, grid, lr, F_b, DX, DY), 0.5 * dt)
     lr.U, lr.S = np.linalg.qr(K, mode="reduced")
-    lr.U /= np.sqrt(grid.dx)
-    lr.S *= np.sqrt(grid.dx)
+    lr.U /= (np.sqrt(grid.dx) * np.sqrt(grid.dy))
+    lr.S *= (np.sqrt(grid.dx) * np.sqrt(grid.dy))
 
     # 1/2 S step
     D1 = computeD(
@@ -423,8 +427,8 @@ def PSI_splitting_strang(
     K = lr.U @ lr.S
     K += 0.5 * dt * RK4(K, lambda K: Kstep1(C1, grid, lr, F_b, DX, DY), 0.5 * dt)
     lr.U, lr.S = np.linalg.qr(K, mode="reduced")
-    lr.U /= np.sqrt(grid.dx)
-    lr.S *= np.sqrt(grid.dx)
+    lr.U /= (np.sqrt(grid.dx) * np.sqrt(grid.dy))
+    lr.S *= (np.sqrt(grid.dx) * np.sqrt(grid.dy))
 
     ### Drop basis for adaptive rank strategy:
     lr, grid = drop_basis_functions(lr, grid, drop_tol)
@@ -443,8 +447,8 @@ def PSI_splitting_strang(
     K = lr.U @ lr.S
     K += 0.5 * dt * RK4(K, lambda K: Kstep2(K, C1, grid, DX, DY), 0.5 * dt)
     lr.U, lr.S = np.linalg.qr(K, mode="reduced")
-    lr.U /= np.sqrt(grid.dx)
-    lr.S *= np.sqrt(grid.dx)
+    lr.U /= (np.sqrt(grid.dx) * np.sqrt(grid.dy))
+    lr.S *= (np.sqrt(grid.dx) * np.sqrt(grid.dy))
 
     # 1/2 S step
     D1 = computeD(
@@ -490,8 +494,8 @@ def PSI_splitting_strang(
     K = lr.U @ lr.S
     K += 0.5 * dt * RK4(K, lambda K: Kstep2(K, C1, grid, DX, DY), 0.5 * dt)
     lr.U, lr.S = np.linalg.qr(K, mode="reduced")
-    lr.U /= np.sqrt(grid.dx)
-    lr.S *= np.sqrt(grid.dx)
+    lr.U /= (np.sqrt(grid.dx) * np.sqrt(grid.dy))
+    lr.S *= (np.sqrt(grid.dx) * np.sqrt(grid.dy))
 
     ### Drop basis for adaptive rank strategy:
     lr, grid = drop_basis_functions(lr, grid, drop_tol)
@@ -505,8 +509,8 @@ def PSI_splitting_strang(
     K = lr.U @ lr.S
     K += 0.5 * dt * RK4(K, lambda K: Kstep3(K, C2, grid), 0.5 * dt)
     lr.U, lr.S = np.linalg.qr(K, mode="reduced")
-    lr.U /= np.sqrt(grid.dx)
-    lr.S *= np.sqrt(grid.dx)
+    lr.U /= (np.sqrt(grid.dx) * np.sqrt(grid.dy))
+    lr.S *= (np.sqrt(grid.dx) * np.sqrt(grid.dy))
 
     # 1/2 S step
     lr.S += 0.5 * dt * RK4(lr.S, lambda S: Sstep3(S, C2, grid), 0.5 * dt)
@@ -530,7 +534,7 @@ def PSI_splitting_strang(
     K = lr.U @ lr.S
     K += 0.5 * dt * RK4(K, lambda K: Kstep3(K, C2, grid), 0.5 * dt)
     lr.U, lr.S = np.linalg.qr(K, mode="reduced")
-    lr.U /= np.sqrt(grid.dx)
-    lr.S *= np.sqrt(grid.dx)
+    lr.U /= (np.sqrt(grid.dx) * np.sqrt(grid.dy))
+    lr.S *= (np.sqrt(grid.dx) * np.sqrt(grid.dy))
 
     return lr, grid, rank_adapted, rank_dropped

DLR_rt/src/lr.py
@@ -580,13 +580,13 @@ def computeD(
     elif dimensions == "2x1d":
         if option_dd == "no_dd":
             if option_coeff == "constant":
-                D1X = lr.U.T @ DX @ lr.U * grid.dx
-                D1Y = lr.U.T @ DY @ lr.U * grid.dy
+                D1X = lr.U.T @ DX @ lr.U * grid.dx * grid.dy
+                D1Y = lr.U.T @ DY @ lr.U * grid.dy * grid.dx
                 D1 = [D1X, D1Y]
             
             elif option_coeff == "space_dep":
-                D1X = lr.U.T @ grid.coeff[0] @ DX @ lr.U * grid.dx
-                D1Y = lr.U.T @ grid.coeff[0] @ DY @ lr.U * grid.dy
+                D1X = lr.U.T @ grid.coeff[0] @ DX @ lr.U * grid.dx * grid.dy
+                D1Y = lr.U.T @ grid.coeff[0] @ DY @ lr.U * grid.dy * grid.dx
                 D1 = [D1X, D1Y]
 
         elif option_dd == "dd":
@@ -596,16 +596,16 @@ def computeD(
                 lr, K_bdry_left, K_bdry_right, K_bdry_bottom, K_bdry_top, grid, DX, DY
             )
             if option_coeff == "constant":
-                D1X = lr.U.T @ DXK * grid.dx
+                D1X = lr.U.T @ DXK * grid.dx * grid.dy
 
-                D1Y = lr.U.T @ DYK * grid.dy
+                D1Y = lr.U.T @ DYK * grid.dy * grid.dx
 
                 D1 = [D1X, D1Y]
             
             elif option_coeff == "space_dep":
-                D1X = lr.U.T @ grid.coeff[0] @ DXK * grid.dx
+                D1X = lr.U.T @ grid.coeff[0] @ DXK * grid.dx * grid.dy
 
-                D1Y = lr.U.T @ grid.coeff[0] @ DYK * grid.dy
+                D1Y = lr.U.T @ grid.coeff[0] @ DYK * grid.dy * grid.dx
 
                 D1 = [D1X, D1Y]
 
@@ -613,8 +613,9 @@ def computeD(
 
 def computeE(lr, grid):
 
-    E1_1 = lr.U.T @ grid.coeff[1] @ lr.U * grid.dx
-    E1_2 = lr.U.T @ grid.coeff[2] @ lr.U * grid.dx
+    E1_1 = lr.U.T @ grid.coeff[1] @ lr.U * grid.dx * grid.dy
+    #multiply with grid.dy as well?
+    E1_2 = lr.U.T @ grid.coeff[2] @ lr.U * grid.dx * grid.dy
     E1 = [E1_1, E1_2]
 
     return E1
@@ -930,12 +931,12 @@ def add_basis_functions(
 
     # QR-decomp
     lr.U, R_U = np.linalg.qr(lr.U, mode="reduced")
-    if option == "x_advection":
+    if dimensions == "1x1d":
         lr.U /= np.sqrt(grid.dx)
         R_U *= np.sqrt(grid.dx)
-    elif option == "y_advection":
-        lr.U /= np.sqrt(grid.dy)
-        R_U *= np.sqrt(grid.dy)
+    elif dimensions == "2x1d":
+        lr.U /= (np.sqrt(grid.dx) * np.sqrt(grid.dy))
+        R_U *= (np.sqrt(grid.dx) * np.sqrt(grid.dy))
     lr.V, R_V = np.linalg.qr(lr.V, mode="reduced")
     if dimensions == "1x1d":
         lr.V /= np.sqrt(grid.dmu)