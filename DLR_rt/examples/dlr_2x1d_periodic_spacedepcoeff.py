fe48a45cffc7265b188d8c17b68677869155545a
Stefan Brunner <Stefan.Brunner@student.uibk.ac.at>
2025-09-08 15:30:08 +0200

    Reorganize dlr_2x1d_periodic_spacedepcoeff.py

--- DLR_rt/examples/dlr_2x1d_periodic_spacedepcoeff.py
+++ DLR_rt/examples/dlr_2x1d_periodic_spacedepcoeff.py
@@ -48,86 +48,48 @@ def integrate(lr0: LR, grid: Grid_2x1d, t_f: float, dt: float,
 
 ### Plotting
 
-Nx = 128
-Ny = 128
-Nphi = 128
+Nx = 252
+Ny = 252
+Nphi = 252
 dt = 0.95 / Nx
-r = 5
-t_f = 0.3
+r = 50
+t_f = 0.05
 fs = 16
 savepath = "plots/"
 method = "lie"
 option_grid = "dd"      # Just changes how gridpoints are chosen
 option_scheme = "upwind"
 
+
 # ### To compare with constant coefficient results
 # c_adv = diags(np.ones(Nx*Ny))
 # c_s = diags(np.zeros(Nx*Ny))
 # c_t = diags(np.zeros(Nx*Ny))
 
-# ### For question 1
-# s = np.ones(Nx*Ny)
-# s[int(Nx*Ny/8*7):]=0
-# t = np.ones(Nx*Ny)
-# t[int(Nx*Ny/8*7):]=10
-# c_s = diags(s)
-# c_t = diags(t)
-
-
-# ### Simplified lattice test setup
-# c_adv_vec = np.ones(Nx*Ny)
-# c_adv = diags(c_adv_vec)
-
-# # Block and grid sizes
-# num_blocks = 8
-# block_size = int(Nx/num_blocks)
-
-# # The pattern of numbers in each row of blocks
-# pattern_s = [1, 0, 1, 0, 1, 0, 1, 1]
-# pattern_t = [1, 10, 1, 10, 1, 10, 1, 1]
-
-# # Create the 8x8 block matrix with pattern repeated in each row
-# c_s_block_matrix = np.array([pattern_s] * num_blocks)
-# c_t_block_matrix = np.array([pattern_t] * num_blocks)
-
-# # Expand each block to be block_size x block_size
-# c_s_matrix = np.kron(c_s_block_matrix, np.ones((block_size, block_size), dtype=int))
-# c_t_matrix = np.kron(c_t_block_matrix, np.ones((block_size, block_size), dtype=int))
-
-# # Change to vector
-# c_s_vec = c_s_matrix.flatten()
-# c_t_vec = c_t_matrix.flatten()
-
-# # Change to diag matrix
-# c_s = diags(c_s_vec)
-# c_t = diags(c_t_vec)
 
 ### Full lattice setup
-
 c_adv_vec = np.ones(Nx*Ny)
 c_adv = diags(c_adv_vec)
 
 # Parameters
-num_blocks = 8        # number of blocks in each row/col
+num_blocks = 7        # number of blocks in each row/col
 block_size = int(Nx/num_blocks)        # size of each block
 
 # Pattern of blocks
-block_pattern_s = np.array([[1,1,1,1,1,1,1,1],
-                            [1,0,1,0,1,0,1,1],
-                            [1,1,0,1,0,1,1,1],
-                            [1,0,1,0,1,0,1,1],
-                            [1,1,0,1,0,1,1,1],
-                            [1,0,1,1,1,0,1,1],
-                            [1,1,1,1,1,1,1,1],
-                            [1,1,1,1,1,1,1,1]])
-block_pattern_t = np.array([[1,1,1,1,1,1,1,1],
-                            [1,10,1,10,1,10,1,1],
-                            [1,1,10,1,10,1,1,1],
-                            [1,10,1,10,1,10,1,1],
-                            [1,1,10,1,10,1,1,1],
-                            [1,10,1,1,1,10,1,1],
-                            [1,1,1,1,1,1,1,1],
-                            [1,1,1,1,1,1,1,1]])
+block_pattern_s = np.array([[1,1,1,1,1,1,1],
+                            [1,0,1,0,1,0,1],
+                            [1,1,0,1,0,1,1],
+                            [1,0,1,1,1,0,1],
+                            [1,1,0,1,0,1,1],
+                            [1,0,1,1,1,0,1],
+                            [1,1,1,1,1,1,1]])
+block_pattern_t = np.array([[1,1,1,1,1,1,1],
+                            [1,10,1,10,1,10,1],
+                            [1,1,10,1,10,1,1],
+                            [1,10,1,1,1,10,1],
+                            [1,1,10,1,10,1,1],
+                            [1,10,1,1,1,10,1],
+                            [1,1,1,1,1,1,1]])
 
 # Expand each block into block_size x block_size
 c_s_matrix = np.kron(block_pattern_s, np.ones((block_size, block_size), dtype=int))
@@ -141,32 +103,25 @@ c_t_vec = c_t_matrix.flatten()
 c_s = diags(c_s_vec)
 c_t = diags(c_t_vec)
 
-# c_adv = diags(np.zeros(Nx*Ny))
-# c_s = diags(np.zeros(Nx*Ny))
-# c_t = diags(np.zeros(Nx*Ny))
 
-# ### To test
-# c_adv = diags(np.zeros(Nx*Ny))
-# c_s = diags(np.zeros(Nx*Ny))
-# c_t = diags(np.zeros(Nx*Ny))
+### Setup grid and initial condition
+grid = Grid_2x1d(Nx, Ny, Nphi, r, _option_dd=option_grid, _coeff=[c_adv, c_s, c_t])
+lr0 = setInitialCondition_2x1d_lr(grid, option_cond="lattice")
+f0 = lr0.U @ lr0.S @ lr0.V.T
 
-num_blocks = 8
-block_size = int(Nx/num_blocks)  
 
-### Calculate source
+# ### Do normal 1 source
+# # Start with all zeros
+# block_matrix = np.zeros((num_blocks, num_blocks))
 
-# Start with all zeros
-block_matrix = np.zeros((num_blocks, num_blocks))
+# # Set block (4,4) to 1
+# block_row = 3
+# block_col = 3
+# block_matrix[block_row, block_col] = 1
 
-# Set block (4,4) to 1
-block_row = 3
-block_col = 3
-block_matrix[block_row, block_col] = 1
+# # Expand to full matrix
+# source = np.kron(block_matrix, np.ones((block_size, block_size)))
 
-# Expand to full matrix
-matrix = np.kron(block_matrix, np.ones((block_size, block_size)))
-
-source = matrix.flatten()[:, None]
 
 # ### Do Gaussian source
 # source = np.zeros((Nx,Ny))
@@ -178,12 +133,42 @@ source = matrix.flatten()[:, None]
 #                         * np.exp(-((grid.X[i] - 0.5) ** 2) / 0.02)
 #                         * np.exp(-((grid.Y[j] - 0.5) ** 2) / 0.02)
 #                     )
-# source = source.flatten()[:, None]
 
-### Do the plotting
-grid = Grid_2x1d(Nx, Ny, Nphi, r, _option_dd=option_grid, _coeff=[c_adv, c_s, c_t])
-lr0 = setInitialCondition_2x1d_lr(grid, option_cond="lattice")
-f0 = lr0.U @ lr0.S @ lr0.V.T
+
+### Do sinus source
+source = np.zeros((Nx,Ny))
+for i in range(3*block_size, 4*block_size):
+    for j in range(3*block_size, 4*block_size):
+        source[i,j] = (
+                        np.sin((grid.X[i] - 3*block_size/Nx) * Nx/block_size * np.pi)
+                        * np.sin((grid.Y[j] - 3*block_size/Ny) 
+                                 * Nx/block_size * np.pi)
+                    )
+        
+
+### Plot source
+extent = [grid.X[0], grid.X[-1], grid.Y[0], grid.Y[-1]]
+fig, axes = plt.subplots(1, 1, figsize=(10, 8))
+
+im = axes.imshow(source.T, extent=extent, origin="lower", cmap="jet")
+axes.set_xlabel("$x$", fontsize=fs)
+axes.set_ylabel("y", fontsize=fs, labelpad=-5)
+axes.set_xticks([0, 0.5, 1])
+axes.set_yticks([0, 0.5, 1])
+axes.tick_params(axis="both", labelsize=fs, pad=10)
+
+cbar_fixed = fig.colorbar(im, ax=axes)
+cbar_fixed.set_ticks([np.min(source), np.max(source)])
+cbar_fixed.ax.tick_params(labelsize=fs)
+
+plt.tight_layout()
+plt.savefig(savepath + "source.pdf")
+
+# Prepare source for code
+source = source.flatten()[:, None]
+
+
+### Run code and do the plotting
 lr, time = integrate(lr0, grid, t_f, dt, source=source, option_scheme=option_scheme)
 f = lr.U @ lr.S @ lr.V.T
 
@@ -201,7 +186,9 @@ extent = [grid.X[0], grid.X[-1], grid.Y[0], grid.Y[-1]]
 
 fig, axes = plt.subplots(1, 1, figsize=(10, 8))
 
-im = axes.imshow(rho0_matrix.T, extent=extent, origin="lower")
+im = axes.imshow(np.log(rho0_matrix.T), extent=extent, origin="lower", 
+                 vmin=np.log(1e-3), vmax=np.log(np.max(rho0_matrix)), 
+                 cmap="jet")
 axes.set_xlabel("$x$", fontsize=fs)
 axes.set_ylabel("y", fontsize=fs, labelpad=-5)
 axes.set_xticks([0, 0.5, 1])
@@ -209,7 +196,7 @@ axes.set_yticks([0, 0.5, 1])
 axes.tick_params(axis="both", labelsize=fs, pad=10)
 
 cbar_fixed = fig.colorbar(im, ax=axes)
-cbar_fixed.set_ticks([np.min(rho0_matrix), np.max(rho0_matrix)])
+cbar_fixed.set_ticks([np.log(1e-3), np.log(np.max(rho0_matrix))])
 cbar_fixed.ax.tick_params(labelsize=fs)
 
 plt.tight_layout()
@@ -217,7 +204,10 @@ plt.savefig(savepath + "2x1d_rho_initial_spacedepcoeff.pdf")
 
 fig, axes = plt.subplots(1, 1, figsize=(10, 8))
 
-im = axes.imshow(rho_matrix.T, extent=extent, origin="lower")
+# im = axes.imshow(rho_matrix.T, extent=extent, origin="lower")
+im = axes.imshow(np.log(rho_matrix.T), extent=extent, origin="lower", 
+                 vmin=np.log(1e-3), vmax=np.log(np.max(rho_matrix)), 
+                 cmap="jet")
 axes.set_xlabel("$x$", fontsize=fs)
 axes.set_ylabel("y", fontsize=fs, labelpad=-5)
 axes.set_xticks([0, 0.5, 1])
@@ -225,7 +215,8 @@ axes.set_yticks([0, 0.5, 1])
 axes.tick_params(axis="both", labelsize=fs, pad=10)
 
 cbar_fixed = fig.colorbar(im, ax=axes)
-cbar_fixed.set_ticks([np.min(rho_matrix), np.max(rho_matrix)])
+# cbar_fixed.set_ticks([np.min(rho_matrix), np.max(rho_matrix)])
+cbar_fixed.set_ticks([np.log(1e-3), np.log(np.max(rho_matrix))])
 cbar_fixed.ax.tick_params(labelsize=fs)
 
 plt.tight_layout()
