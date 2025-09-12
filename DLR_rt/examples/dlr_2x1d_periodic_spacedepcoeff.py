874618bad0a64fbc6a918c70e215b6e1cc2f5eeb
Stefan Brunner <Stefan.Brunner@student.uibk.ac.at>
2025-09-12 10:22:31 +0200

    Add hohlraum setup (still need to implement correct bc)

--- DLR_rt/examples/dlr_2x1d_periodic_spacedepcoeff.py
+++ DLR_rt/examples/dlr_2x1d_periodic_spacedepcoeff.py
@@ -50,18 +50,18 @@ def integrate(lr0: LR, grid: Grid_2x1d, t_f: float, dt: float,
 
 ### Plotting
 
-Nx = 252
-Ny = 252
-Nphi = 252
-dt = 0.02
-r = 50
-t_f = 0.7
+Nx = 200
+Ny = 200
+Nphi = 200
+dt = 0.95 / Nx
+r = 30
+t_f = 0.4
 fs = 16
 savepath = "plots/"
 method = "lie"
 option_grid = "dd"      # Just changes how gridpoints are chosen
 option_scheme = "upwind"
-option_timescheme = "impl_Euler"
+option_timescheme = "RK4"
 
 
 # ### To compare with constant coefficient results
@@ -70,47 +70,76 @@ option_timescheme = "impl_Euler"
 # c_t = diags(np.zeros(Nx*Ny))
 
 
-### Full lattice setup
+# ### Full lattice setup
+# c_adv_vec = np.ones(Nx*Ny)
+# c_adv = diags(c_adv_vec)
+
+# # Parameters
+# num_blocks = 7        # number of blocks in each row/col
+# block_size = int(Nx/num_blocks)        # size of each block
+
+# # Pattern of blocks
+# block_pattern_s = np.array([[1,1,1,1,1,1,1],
+#                             [1,0,1,0,1,0,1],
+#                             [1,1,0,1,0,1,1],
+#                             [1,0,1,1,1,0,1],
+#                             [1,1,0,1,0,1,1],
+#                             [1,0,1,1,1,0,1],
+#                             [1,1,1,1,1,1,1]])
+# block_pattern_t = np.array([[1,1,1,1,1,1,1],
+#                             [1,10,1,10,1,10,1],
+#                             [1,1,10,1,10,1,1],
+#                             [1,10,1,1,1,10,1],
+#                             [1,1,10,1,10,1,1],
+#                             [1,10,1,1,1,10,1],
+#                             [1,1,1,1,1,1,1]])
+# # block_pattern_s = np.array([[1,0,1,1,1,0,1],
+# #                             [1,0,1,1,1,0,1],
+# #                             [1,0,1,1,1,0,1],
+# #                             [1,0,1,1,1,0,1],
+# #                             [1,0,1,1,1,0,1],
+# #                             [1,0,1,1,1,0,1],
+# #                             [1,0,1,1,1,0,1]])
+# # block_pattern_t = np.array([[1,10,1,1,1,10,1],
+# #                             [1,10,1,1,1,10,1],
+# #                             [1,10,1,1,1,10,1],
+# #                             [1,10,1,1,1,10,1],
+# #                             [1,10,1,1,1,10,1],
+# #                             [1,10,1,1,1,10,1],
+# #                             [1,10,1,1,1,10,1]])
+
+# # Expand each block into block_size x block_size
+# c_s_matrix = np.kron(block_pattern_s, np.ones((block_size, block_size), dtype=int))
+# c_t_matrix = np.kron(block_pattern_t, np.ones((block_size, block_size), dtype=int))
+
+# # Change to vector
+# c_s_vec = c_s_matrix.flatten()
+# c_t_vec = c_t_matrix.flatten()
+
+# # Change to diag matrix
+# c_s = diags(c_s_vec)
+# c_t = diags(c_t_vec)
+
+
+### Full hohlraum setup
+
 c_adv_vec = np.ones(Nx*Ny)
 c_adv = diags(c_adv_vec)
 
-# Parameters
-num_blocks = 7        # number of blocks in each row/col
-block_size = int(Nx/num_blocks)        # size of each block
+c_s_matrix = np.zeros((Nx,Ny))
+c_t_matrix = np.zeros((Nx,Ny))
 
-# Pattern of blocks
-block_pattern_s = np.array([[1,1,1,1,1,1,1],
-                            [1,0,1,0,1,0,1],
-                            [1,1,0,1,0,1,1],
-                            [1,0,1,1,1,0,1],
-                            [1,1,0,1,0,1,1],
-                            [1,0,1,1,1,0,1],
-                            [1,1,1,1,1,1,1]])
-block_pattern_t = np.array([[1,1,1,1,1,1,1],
-                            [1,10,1,10,1,10,1],
-                            [1,1,10,1,10,1,1],
-                            [1,10,1,1,1,10,1],
-                            [1,1,10,1,10,1,1],
-                            [1,10,1,1,1,10,1],
-                            [1,1,1,1,1,1,1]])
-# block_pattern_s = np.array([[1,0,1,1,1,0,1],
-#                             [1,0,1,1,1,0,1],
-#                             [1,0,1,1,1,0,1],
-#                             [1,0,1,1,1,0,1],
-#                             [1,0,1,1,1,0,1],
-#                             [1,0,1,1,1,0,1],
-#                             [1,0,1,1,1,0,1]])
-# block_pattern_t = np.array([[1,10,1,1,1,10,1],
-#                             [1,10,1,1,1,10,1],
-#                             [1,10,1,1,1,10,1],
-#                             [1,10,1,1,1,10,1],
-#                             [1,10,1,1,1,10,1],
-#                             [1,10,1,1,1,10,1],
-#                             [1,10,1,1,1,10,1]])
+# Set c_t for absorbing parts
+for i in range(Nx):
+    for j in range(Ny):
 
-# Expand each block into block_size x block_size
-c_s_matrix = np.kron(block_pattern_s, np.ones((block_size, block_size), dtype=int))
-c_t_matrix = np.kron(block_pattern_t, np.ones((block_size, block_size), dtype=int))
+        if j <= 0.05*Ny or j >= 0.95*Ny:    # upper and lower blocks
+            c_t_matrix[j,i] = 1
+
+        else:
+            if (i >= 0.95*Nx or i<=0.05*Nx and (0.25*Ny <= j <= 0.75*Ny)
+                or (0.25*Nx <= i <= 0.75*Nx) and (0.25*Ny <= j <= 0.75*Ny)):
+                c_t_matrix[j,i] = 1
 
 # Change to vector
 c_s_vec = c_s_matrix.flatten()
@@ -131,7 +160,7 @@ f0 = lr0.U @ lr0.S @ lr0.V.T
 extent = [grid.X[0], grid.X[-1], grid.Y[0], grid.Y[-1]]
 fig, axes = plt.subplots(1, 1, figsize=(10, 8))
 
-im = axes.imshow(c_s_matrix, extent=extent, origin="lower", cmap="jet")
+im = axes.imshow(c_t_matrix, extent=extent, origin="lower", cmap="jet")
 axes.set_xlabel("$x$", fontsize=fs)
 axes.set_ylabel("y", fontsize=fs, labelpad=-5)
 axes.set_xticks([0, 0.5, 1])
@@ -139,24 +168,24 @@ axes.set_yticks([0, 0.5, 1])
 axes.tick_params(axis="both", labelsize=fs, pad=10)
 
 cbar_fixed = fig.colorbar(im, ax=axes)
-cbar_fixed.set_ticks([np.min(c_s_matrix), np.max(c_s_matrix)])
+cbar_fixed.set_ticks([np.min(c_t_matrix), np.max(c_t_matrix)])
 cbar_fixed.ax.tick_params(labelsize=fs)
 
 plt.tight_layout()
 plt.savefig(savepath + "lattice1.pdf")
 
 
-### Do normal 1 source
-# Start with all zeros
-block_matrix = np.zeros((num_blocks, num_blocks))
+# ### Do normal 1 source
+# # Start with all zeros
+# block_matrix = np.zeros((num_blocks, num_blocks))
 
-# Set block (4,4) to 1
-block_row = 3
-block_col = 3
-block_matrix[block_row, block_col] = 1
+# # Set block (4,4) to 1
+# block_row = 3
+# block_col = 3
+# block_matrix[block_row, block_col] = 1
 
-# Expand to full matrix
-source = np.kron(block_matrix, np.ones((block_size, block_size)))
+# # Expand to full matrix
+# source = np.kron(block_matrix, np.ones((block_size, block_size)))
 
 
 # ### Do Gaussian source
@@ -180,13 +209,17 @@ source = np.kron(block_matrix, np.ones((block_size, block_size)))
 #                         * np.sin((grid.Y[j] - 3*block_size/Ny) 
 #                                  * Ny/block_size * np.pi)
 #                     )
-        
+
+
+### Do hohlraum source (technically it should be inflow, not source at gridpoints)
+source = np.zeros((Nx,Ny))
+source[:,0] = 1
 
 ### Plot source
 extent = [grid.X[0], grid.X[-1], grid.Y[0], grid.Y[-1]]
 fig, axes = plt.subplots(1, 1, figsize=(10, 8))
 
-im = axes.imshow(source.T, extent=extent, origin="lower", cmap="jet")
+im = axes.imshow(source, extent=extent, origin="lower", cmap="jet")
 axes.set_xlabel("$x$", fontsize=fs)
 axes.set_ylabel("y", fontsize=fs, labelpad=-5)
 axes.set_xticks([0, 0.5, 1])
